d8675d59814d8590e894a73113eac00f89872302
Gabriel Donadel Dall'Agnol <donadeldev@gmail.com>
2021-07-21 13:13:23 -0300

    feat: Update script to use git email

--- script.sh
+++ script.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 counter="$(cat counter.txt)"
+git_email="$(git config user.email)"
 
 createPR(){
   counter=$((counter+1))
 
   echo $counter > counter.txt
 
-  git checkout -b "$counter"
+  git checkout -b "$git_email-$counter"
   echo $counter > "$counter.txt"
 
   git add "$counter.txt"
   git commit -m "Add file $counter.txt"
-  git push --set-upstream origin $counter
+  git push --set-upstream origin "$git_email-$counter"
   hub pull-request -m "Add file $counter.txt" -b gabrieldonadel:master
   git checkout master
 }
